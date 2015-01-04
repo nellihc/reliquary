90a588d5bf45a5d1ad9a1d173d6bf7477dbe1f68
nellihc <joe.hasselstrom@gmail.com>
2015-01-04 01:39:09 -0500

    Update and rename cap.sh to capture.sh

--- capture.sh
+++ capture.sh
@@ -2,15 +2,19 @@
 
 TSHARK=`which tshark`
 AIRMON_NG=`which airmon-ng`
-CLIENT_STATION=$1
 CAPTURE_FILE_NAME=traffic.csv
 
-# create mon0 interface
+# create mon0 interface to capture on
 $AIRMON_NG start wlan1
 
-# this captures 802.11 probe requests, probe responses
-tshark -i mon0 -Y "wlan.fc.type_subtype==4 || wlan.fc.type_subtype==5" \
-                -T fields -e frame.time                  \
+# this script captures 802.11 probe requests(4), probe responses(5)
+# authentication requests/responses(11),
+# association requests(0), assocation responses(1), 
+# disassociation updates (10)
+tshark -i mon0 -Y "wlan.fc.type_subtype==4  || wlan.fc.type_subtype==5 || \  
+                   wlan.fc.type_subtype==11 || wlan.fc.type_subtype==0 || \
+                   wlan.fc.type_subtype==1  || wlan.fc.type_subtype==10   \
+                          -T fields -e frame.time        \
                           -e wlan.fc.type_subtype        \
                           -e wlan.sa                     \
                           -e wlan.da                     \
