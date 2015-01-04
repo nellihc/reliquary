9499a05cca00932d185ba3611c466107376d1607
nellihc <joe.hasselstrom@gmail.com>
2015-01-04 02:04:32 -0500

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -18,12 +18,16 @@ chmod 777 ./$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME
 
 # create mon0 interface to capture on
 # make sure to verify if we should start wlan1 (internal) or maybe wlan2 (alfa)
-$AIRMON_NG start wlan1 $CAPTURE_CHANNEL
+#$AIRMON_NG start wlan1 
 
 # this script captures 802.11 probe requests(4), probe responses(5)
 # authentication requests/responses(11),
 # association requests(0), assocation responses(1), 
 # disassociation updates (10)
+
+###
+# ASSUMES mon0 is created
+###
 tshark -i mon0 -Y "wlan.fc.type_subtype==4 || wlan.fc.type_subtype==5 || wlan.fc.type_subtype==11 || wlan.fc.type_subtype==0 || wlan.fc.type_subtype==1 || wlan.fc.type_subtype==10" \
                           -T fields -e frame.time        \
                           -e wlan.fc.type_subtype        \
