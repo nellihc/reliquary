89651a9371a711f7299b42862aa214959ffe445d
nellihc <joe.hasselstrom@gmail.com>
2015-01-03 20:48:36 -0500

    Create cap.sh

---
+++ cap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TSHARK=`which tshark`
+CLIENT_STATION=$1
+CAPTURE_FILE_NAME=traffic.csv
+
+# this captures 802.11 probe requests, probe responses
+tshark -i mon0 -T fields -e frame.time \
+                          -e wlan.fc.type_subtype \            
+                          -e wlan.sa \
+                          -e wlan.da \
+                          -e wlan_mgt.ssid \
+                          -e radiotap.channel.freq \
+                          -e radiotap.dbm_antsignal \
+                          -E separator="," \
+                          -E header=y \
+                          -E quote=d \
+                          -E occurrence=f \
+                          > $CAPTURE_FILE_NAME
