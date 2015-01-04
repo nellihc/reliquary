af716f13fcbbb636f3d2e1a42de0c8d819eb09ce
nellihc <joe.hasselstrom@gmail.com>
2015-01-03 20:49:24 -0500

    editing sapces after subtype fc

--- cap.sh
+++ cap.sh
@@ -6,7 +6,7 @@ CAPTURE_FILE_NAME=traffic.csv
 
 # this captures 802.11 probe requests, probe responses
 tshark -i mon0 -T fields -e frame.time \
-                          -e wlan.fc.type_subtype \            
+                          -e wlan.fc.type_subtype \
                           -e wlan.sa \
                           -e wlan.da \
                           -e wlan_mgt.ssid \
