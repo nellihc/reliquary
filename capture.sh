43f36561292e1614f9c6cda5e971b9c2a657ea80
nellihc <joe.hasselstrom@gmail.com>
2015-01-04 01:50:55 -0500

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -6,14 +6,15 @@ CAPTURE_FILE_DIRECTORY=output
 CAPTURE_FILE_NAME=traffic.csv
 
 # create our file if it does not exist
-if [ ! -e "$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME" ] ; then
-    touch "$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME"
+if [ ! -e "./$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME" ] ; then
+    touch "./$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME"
 fi
 
 # modify permissions
-chmod 777 $CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME
+chmod 777 ./$CAPTURE_FILE_DIRECTORY/$CAPTURE_FILE_NAME
 
 # create mon0 interface to capture on
+# make sure to verify if we should start wlan1 (internal) or maybe wlan2 (alfa)
 $AIRMON_NG start wlan1
 
 # this script captures 802.11 probe requests(4), probe responses(5)
