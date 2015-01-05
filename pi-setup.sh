0c2ca27d4f09761807dcc69446e65095cf30a863
nellihc <joe.hasselstrom@gmail.com>
2015-01-04 22:43:45 -0500

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -3,13 +3,17 @@
 
 apt-get update
 apt-get upgrade 
-apt-get install libssl--dev -y
-apt-get install libnl-3-dev -y
+apt-get install libssl--dev libnl-3-dev iw macchanger libnl-genl-3-dev libnl-3-dev -y
+
 
 cd ~ && wget http://download.aircrack-ng.org/aircrack-ng-1.2-rc1.tar.gz
 tar -zxvf aircrack-ng-1.2-rc1.tar.gz
 cd aircrack-ng-1.2-rc1
 make
 make install
+
+# done 
+airodump-ng-oui-update
+
 
 
