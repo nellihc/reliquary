2ac012586205739799a8b8200bf640531af65679
nellihc <joe.hasselstrom@gmail.com>
2015-02-07 21:26:20 -0500

    Update open-ssid-cap.sh

--- open-ssid-cap.sh
+++ open-ssid-cap.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# kill bad processes
+killall dhclient
+killall wpa_supplicant
+killall wpa_cli
+killall ifplugd
+
+# start airmon-ng
+airmon-ng start wlan0
+
+# collect beacons that support open wifi
+tshark -i mon0 -R "(wlan.fc.type_subtype == 0x0008) && (wlan_mgt.fixed.capabilities.privacy == 0)" >> /tmp/open-wifi.txt
+
